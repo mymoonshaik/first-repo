606f94adb72346f7f20dbc9c3e490d370ddeadd0
mymoons <mymoons@INDHYDEWS001.planit.local>
2018-07-30 12:58:42 +0530

    First commit

---
+++ src/practice1/Example1.java
@@ -0,0 +1,9 @@
+package practice1;
+
+public class Example1 {
+	public static void main(String[] args) {
+		System.out.println("jhfhsjfhzxfhfgshfs");
+		System.out.println("jkvsdjkvskfvios");
+	}
+
+}
